39428db52b791f8c5dc6c5f499e17f2d1051604b
Josh Benfell <joshuabenfell@hotmail.co.nz>
2021-07-22 02:27:20 +1200

    A1

---
+++ Assignment1/A1.m
@@ -0,0 +1,20 @@
+clear;
+
+figure(1);
+G_1 = tf([20 20 10], [1 11 10 0]);
+%step(feedback(G_1,1));
+nyquist(100000*G_1);
+
+figure(2);
+G_2 = tf([20 20 10], [1 9 -10 0]);
+%step(feedback(G_2,1))
+nyquist(G_2);
+
+
+figure(3);
+G_3 = tf([1 0 3], [1 2 1]);
+nyquist(G_3);
+
+figure(4);
+G_4 = tf([3 3], [1 -10 0]);
+nyquist(G_4);
