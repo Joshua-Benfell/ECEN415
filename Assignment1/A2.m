5826a5c3e1730731dab3835e6fcec42fd9296f5c
Josh Benfell <joshuabenfell@hotmail.co.nz>
2021-07-24 01:11:06 +1200

    Complete A2

--- Assignment1/A2.m
+++ Assignment1/A2.m
@@ -15,9 +15,12 @@ step(feedback(2.298* G, 1));
 close all 
 G = zpk([], -2, 4);
 figure(1)
-for i = 0.1:0.1:0.7
+delays = 0.1:0.1:0.7;
+for i = delays
     G.IODelay = i;
     nyquist(G);
     hold on
 end
+legendCell = cellstr(num2str(delays', 'Delay = %.1f'));
+legend(legendCell)
 
