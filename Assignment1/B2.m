f225c567a0d91a7fad1651c92c81541f61feaf3e
Joshua Benfell <joshuabenfell@hotmail.co.nz>
2021-07-27 23:26:58 +1200

    Done

--- Assignment1/B2.m
+++ Assignment1/B2.m
@@ -1,1 +1,24 @@
+clear
+close all
+figure(1)
+subplot(1,2,1)
 G = tf(15, [1 3 2])
+rlocus(G)
+title("G(s)")
+subplot(1,2,2)
+s = tf('s');
+ts = -100000;
+sample = (1 - pade(exp(ts*s),1))/s
+rlocus(G*sample)
+title("G(s) Sampled")
+figure(2)
+title("G(s)")
+margin(G)
+figure(3)
+ts = -0.176999;
+%ts = -0.000001
+sample = (1 - pade(exp(ts*s),1))/s;
+rlocus(G * sample)
+figure(4)
+title("G(s) Sampled")
+margin(G * sample)
